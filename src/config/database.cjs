494d7dc52f7f2746f1d82e909dd0c120a44ad743
Deivid Santos Lahasse <deividlahasse@gmail.com>
2026-08-05 20:51:58 -0300

    adiciona configuração SSL para conexão com Neon

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,13 +1,35 @@
+// module.exports = {
+//     dialect: process.env.DB_DIALECT,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     username: process.env.DB_USERNAME,
+//     password:process.env.DB_PASSWORD ,
+//     database:process.env.DB_DATABASE,
+//     define:{
+//         timestamps:true,
+//         underscored:true,
+//         underscoredAll: true,
+//     }
+// }
+
+
+
 module.exports = {
     dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
-    password:process.env.DB_PASSWORD ,
-    database:process.env.DB_DATABASE,
-    define:{
-        timestamps:true,
-        underscored:true,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    define: {
+        timestamps: true,
+        underscored: true,
         underscoredAll: true,
     }
 }
