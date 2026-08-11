1bfbeca7e104f71b1abbab4b4f7ccb8e8ea9d320
Deivid Santos Lahasse <deividlahasse@gmail.com>
2026-08-11 10:17:28 -0300

    fix: resolve Sequelize dialect error in database connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,35 @@
+// import mongoose from 'mongoose';
+// import { Sequelize } from 'sequelize';
+// import Category from '../app/models/Category.js';
+// import Product from '../app/models/Product.js';
+// import User from '../app/models/User.js';
+// import databaseConfig from '../config/database.cjs';
+
+// const models = [User, Product, Category];
+
+// class Database {
+//   constructor() {
+//     this.init();
+//     this.mongo();
+//   }
+
+//   init() {
+//     this.connection = new Sequelize(databaseConfig);
+//     models
+//       .map((model) => model.init(this.connection))
+//       .map(
+//         (model) => model.associate && model.associate(this.connection.models),
+//       );
+//   }
+
+//   mongo() {
+//     this.mongooseConncetion = mongoose.connect(
+//       process.env.MONGO_URL
+//     );
+//   }
+// }
+
+// export default new Database();
 import mongoose from 'mongoose';
 import { Sequelize } from 'sequelize';
 import Category from '../app/models/Category.js';
@@ -14,7 +46,24 @@ class Database {
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
+    // Pega o ambiente atual (development por padrão)
+    const env = process.env.NODE_ENV || 'development';
+    const config = databaseConfig[env];
+
+    // Instancia o Sequelize passando as credenciais desestruturadas e garantindo o dialect
+    this.connection = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      {
+        host: config.host,
+        port: config.port,
+        dialect: 'postgres',
+        logging: false,
+        define: config.define,
+      },
+    );
+
     models
       .map((model) => model.init(this.connection))
       .map(
@@ -23,9 +72,7 @@ class Database {
   }
 
   mongo() {
-    this.mongooseConncetion = mongoose.connect(
-      process.env.MONGO_URL
-    );
+    this.mongooseConncetion = mongoose.connect(process.env.MONGO_URL);
   }
 }
 
